0f3baf80a2ffabfa7e4b34d72ce7406c400be9f9
yuito-kijima <pmppmppmppmppmp@yahoo.co.jp>
2025-06-03 16:27:53 +0900

    一覧表示をJDBC版にアップデート

--- controller/FishListServlet.java
+++ controller/FishListServlet.java
@@ -4,22 +4,46 @@ import java.util.*;
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
+import java.sql.*;
 
 public class FishListServlet extends HttpServlet {
-    private static List<Fish> fishList = new ArrayList<>();
-
+    private static final String DB_URL = "jbdc:derby:fishdb";
+    private static final String DB_USER = "";
+    private static final String DB_PASS = "";
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res)
         throws ServletException, IOException {
-        List<Fish> fishList = getFistList();
+        List<Fish> fishList = new ArrayList<>();
+
+        try {
+            Class.forName("org.apach.derby.jdbc.EmbeddeDriver");
+
+            Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+
+            String sql = "SELECT id, fish_name, price, feature, status, stock FROM fish";
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while (rs.next()) {
+                Fish fish = new Fish(
+                    rs.getString("fish_name"),
+                    rs.getInt("price"),
+                    rs.getString("feature"),
+                    rs.getString("status"),
+                    rs.getInt("stock")
+                );
+                fishList.add(fish);
+            }
+
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         req.setAttribute("fishList", fishList);
-
         RequestDispatcher dispatcher = req.getRequestDispatcher("/list.jsp");
         dispatcher.forward(req, res);
-    }
-
-    public static List<Fish> getFistList() {
-        return fishList;
     }
 }
